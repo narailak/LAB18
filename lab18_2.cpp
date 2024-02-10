27a147bac75785ea397efc350f412d1d9f44b8ba
narailak <oatdev54@gmil.com>
2024-02-11 00:02:16 +0700

    done

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,12 +1,33 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct Rect{
-	double x,y,w,h;
+struct Rect
+{
+	double x, y, w, h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *R1, Rect *R2)
+{
+	double result, xl, xr, yt, yb;
+	if (R1->y < R2->y)
+		yt = R1->y;
+	else
+		yt = R2->y;
+	if (R1->y - R1->h < R2->y - R2->h)
+		yb = R2->y - R2->h;
+	else
+		yb = R1->y - R1->h;
+	if (R1->x > R2->x)
+		xl = R1->x;
+	else
+		xl = R2->x;
+	if (R1->x + R1->w < R2->x + R2->w)
+		xr = R1->x + R1->w;
+	else
+		xr = R2->x + R2->w;
+	result = (yt - yb) * (xr - xl);
+	if (result >= 0)
+		return result;
+	else
+		return 0;
 }
